97a86827abe91bd7726cb5d3d9a0d7f2b17c85ff
coolkiran2006 <coolkiran2006@gmail.com>
2021-02-11 01:00:12 +0530

    test change

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-            Welcome to the ACME World.  This is Complete New World!!!!!.  Experience the Change.
+            Welcome to the ACME World. Experience the Change.
   <!-- END -->
 
   </div>
